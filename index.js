45e7462041adbaec9f80c9d13725f6789bc92c85
C1323113 Lucas Palhano Da Silva Ribeiro <c1323113@interno.bb.com.br>
2021-11-24 15:58:48 -0300

    ajustes iniciais

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-history.pushState = (f => function pushState() {
-    var ret = f.apply(this, arguments)
-    window.dispatchEvent(new Event('pushstate'))
-    window.dispatchEvent(new Event('locationchange'))
-    return ret
-})(history.pushState)
+const monitor = require('./lib/node-monitor');
 
-history.replaceState = (f => function replaceState() {
-    var ret = f.apply(this, arguments)
-    window.dispatchEvent(new Event('replacestate'))
-    window.dispatchEvent(new Event('locationchange'))
-    return ret
-})(history.replaceState)
-
-window.addEventListener('popstate', () => {
-    window.dispatchEvent(new Event('locationchange'))
-})
-
-window.addEventListener('locationchange', function() {
-    console.log('location changed!')
-})
-
-window.addEventListener('hashchange', () => { console.log('location changed!') })
+module.exports = monitor;
