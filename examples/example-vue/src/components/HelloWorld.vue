89d6f498ab33fc63ff30c025c2ac37a55f284209
Luiz Oliveira <ziuloliveira@gmail.com>
2022-01-26 18:53:53 -0300

    include complete vue example
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

---
+++ examples/example-vue/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="app level">
+    <div class="level-item is-justify-content-center">
+      <a
+        @click="updateHash"
+        class="button is-primary is-outlined"
+        style="margin-top: 25%"
+        >Update hash</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import NodeMonitor from '@labbsr0x/node-monitor'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  created() {
+    const monitor = new NodeMonitor()
+    console.log(monitor)
+  },
+  methods: {
+    async updateHash() {
+      const url = new URL(window.location);
+      url.hash = Math.random()
+        .toString(36)
+        .replace(/[^a-z]+/g, "")
+        .substr(0, 5);
+      window.history.pushState({}, "Teste Vue", url);
+    },
+  },
+}
+</script>
+
